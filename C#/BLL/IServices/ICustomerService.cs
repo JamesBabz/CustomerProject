196d9853b623240d7f5c77b6bdb5415e5f2e470b
Enemark96 <je.enemark96@gmail.com>
2017-10-25 12:45:32 +0200

    IServices for all entities
    IServices for all entities

---
+++ C#/BLL/IServices/ICustomerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BLL.BusinessObjects;
+
+namespace BLL.IServices
+{
+    public interface ICustomerService
+    {
+        //C
+        CustomerBO Create(CustomerBO cust);
+        List<CustomerBO> AddCustomers(List<CustomerBO>customers);
+        //R
+        List<CustomerBO> GetAll();
+        //U
+        CustomerBO Update(CustomerBO cust);
+        //D
+        CustomerBO Delete(int id);
+    }
+}
---
+++ C#/BLL/IServices/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BLL.BusinessObjects;
+using DAL.Entities;
+
+namespace BLL.IServices
+{
+    public interface IProductService
+    {
+        //C
+        ProductBO Create(ProductBO product);
+        List<ProductBO> AddProducts(List<ProductBO> products);
+        //R
+        List<ProductBO> GetAll();
+        //U
+        ProductBO Update(ProductBO product);
+        //D
+        ProductBO Delete(int id);
+
+    }
+}
