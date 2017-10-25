4ea71c65e3ccd73d277ed08d2c8ce40efefa2759
thomasmeyerh <thom953b@easv365.dk>
2017-10-25 14:33:44 +0200

    Setup for URL
    Setup for URL

--- Angular/CustomerProject/src/app/app.module.ts
+++ Angular/CustomerProject/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customers/customer/customer.component';
+import {CustomerService} from './customers/shared/customer.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CustomerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
