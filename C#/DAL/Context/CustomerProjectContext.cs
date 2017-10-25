eda9003ed536646714b47ac8d7d4a3020098c039
JamesBabz <web.fuhlendorff@gmail.com>
2017-10-25 12:06:52 +0200

    customer repository

---
+++ C#/DAL/Context/CustomerProjectContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context
+{
+    class CustomerProjectContext : DbContext
+    {
+        public CustomerProjectContext(DbContextOptions<CustomerProjectContext> options): base(options)  { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        static DbContextOptions<CustomerProjectContext> options =
+            new DbContextOptionsBuilder<CustomerProjectContext>()
+                .UseInMemoryDatabase("TheDB")
+                .Options;
+
+        public CustomerProjectContext() : base(options)
+        {
+
+        }
+
+        //public DbSet<Entity> Entites { get; set; }
+
+    }
+}
---
+++ C#/DAL/Repositories/CustomerRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+
+namespace DAL.Repositories
+{
+    class CustomerRepository : IRepository<Customer>
+    {
+        public Customer Create(Customer ent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAllById(List<int> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
