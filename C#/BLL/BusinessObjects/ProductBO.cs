f2b6dfce41f81785875819f0367e44cf7d56846a
Enemark96 <je.enemark96@gmail.com>
2017-10-25 12:01:09 +0200

    Entities
    Classes for entities and BOs

---
+++ C#/BLL/BusinessObjects/ProductBO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.BusinessObjects
+{
+    public class ProductBO : IBusinessObject
+    {
+        public int Id { get; internal set; }
+
+        public string Name { get; set; }
+
+        public string ListPrice { get; set; }
+
+    }
+}
