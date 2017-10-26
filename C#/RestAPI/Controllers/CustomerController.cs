ef40b9b00542fe45b7f96f26c524bbfec12fbd64
Enemark96 <je.enemark96@gmail.com>
2017-10-26 13:16:35 +0200

    CRUD IN RESTAPI
    made the update and delete function inside the controller

--- C#/RestAPI/Controllers/CustomerController.cs
+++ C#/RestAPI/Controllers/CustomerController.cs
@@ -40,5 +40,32 @@ namespace RestAPI.Controllers
 	        }
 	        return Ok(facade.CustomerService.Create(customer));
 	    }
+
+	    // PUT: api/customer/5
+	    [HttpPut("{id}")]
+	    public IActionResult Put(int id, [FromBody]CustomerBO cust)
+	    {
+	        if (id != cust.Id)
+	        {
+	            return StatusCode(405, "Path id does not match customer ID json object");
+	        }
+	        try
+	        {
+	            return Ok(facade.CustomerService.Update(cust));
+	        }
+	        catch (InvalidOperationException e)
+	        {
+	            return StatusCode(404, e.Message);
+	        }
+
+	    }
+
+        // DELETE api/customer/5
+        [HttpDelete("{id}")]
+	    public void Delete(int id)
+	    {
+            facade.CustomerService.Delete(id);
+
+	    }
     }
 }
