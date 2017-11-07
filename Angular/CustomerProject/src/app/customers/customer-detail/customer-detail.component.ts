34dad1bfb13c682cf2880c42bbe8f45d4c4afd08
thomasmeyerh <thom953b@easv365.dk>
2017-11-07 11:50:28 +0100

    Customer can delete account

--- Angular/CustomerProject/src/app/customers/customer-detail/customer-detail.component.ts
+++ Angular/CustomerProject/src/app/customers/customer-detail/customer-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Customer} from '../shared/customer.model';
 import {CustomerService} from '../shared/customer.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {environment} from '../../../environments/environment';
+
 
 @Component({
   selector: 'app-customer-detail',
@@ -11,11 +13,18 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class CustomerDetailComponent implements OnInit {
 
   customer: Customer;
-  constructor(private customerService: CustomerService, private router: Router, private route: ActivatedRoute ) { }
+  inputAddress: string;
+  inputLastname: string;
+  inputFirstname: string;
+  constructor(private customerService: CustomerService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap
       .switchMap(params => this.customerService.getCustomerById(+params.get('id'))).subscribe(Customer => this.customer = Customer);
+  }
+
+  deleteCustomer() {
+    this.customerService.deleteCustomerById(this.customer.id).subscribe(Customer => this.customer = Customer);
   }
 
 }
