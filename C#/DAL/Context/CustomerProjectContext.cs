f242deacaa190ce34d7a6e3bb2351e3aea6432c1
JamesBabz <web.fuhlendorff@gmail.com>
2017-11-13 12:57:20 +0100

    small fixes
    small fixes

--- C#/DAL/Context/CustomerProjectContext.cs
+++ C#/DAL/Context/CustomerProjectContext.cs
@@ -13,28 +13,22 @@ namespace DAL.Context
         public DbSet<Cart> Cart { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
-
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer(
-                    @"Server=tcp:ateamcustomer.database.windows.net,1433;Initial Catalog=ATeamCustomerDb;Persist Security Info=False;User ID=admn;Password=Admin1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                    @"Server=tcp:ateamcustomer.database.windows.net,1433;Initial Catalog=ATeamCustomerDb;Persist Security Info=False;User ID=admn;
+                    Password=Admin1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Cart> Cart { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<CartItem> CartItems { get; set; }
             }
-        
-}
+        }
 
 
 
        
-    }
+        }
 }
